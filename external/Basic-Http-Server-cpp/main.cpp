9842fd87519b337a61a3defa85d36f542aca0dd0
mendax0110 <adrian.goessl@outlook.com>
2023-11-09 22:16:11 +0100

    Details: This commit includes several enhancements to the server setup and connection handling in the main.cpp file.
    The changes include:
    - Improved error handling: The server now has better mechanisms for dealing with potential errors during startup or connection acceptance.
    - Fixed threading: The threading issues that were causing performance bottlenecks and potential race conditions have been addressed.
    - Pointer usage: The code now uses pointers more effectively, reducing the risk of memory leaks and improving overall performance.

--- external/Basic-Http-Server-cpp/main.cpp
+++ external/Basic-Http-Server-cpp/main.cpp
@@ -33,59 +33,57 @@
 #include "html_parser.h"
 
 std::mutex QueueLock;
+std::condition_variable cv;
 std::queue<int> event_queue;
 std::mutex DictionaryLock;
 std::mutex CoutLock;
 
-/// @brief This is the class for the website_handler \name website_handler
 class website_handler
 {
 private:
     std::set<std::string> dictionary;
-    std::map<std::string, char *> page;
+    std::map<std::string, std::unique_ptr<char[]>> page;
 
-    /// @brief This is the method to read a file \name readFile
-    /// @param fileName This is the name of the file to be read
-    /// @return This is the contents of the file, or NULL if failed
-    char *readFile(const char *fileName) 
+    /// @brief This is the read file method, to read the file \name readFile
+    /// @param fileName This is the file name which is to be read
+    /// @return This returns the file content
+    char *readFile(const char *fileName)
     {
         FILE *file = fopen(fileName, "r");
-        if (file == NULL) 
+        if (file == NULL)
         {
             perror("Error opening file");
-            return NULL;
+            return nullptr;
         }
 
-        // Calculate the file size for dynamic memory allocation
         fseek(file, 0, SEEK_END);
         long fileSize = ftell(file);
         rewind(file);
 
-        // Check for an empty file
-        if (fileSize <= 0) 
+        if (fileSize <= 0)
         {
             fclose(file);
-            return NULL;
+            return nullptr;
         }
 
-        char *code = (char *)malloc(fileSize + 1); // +1 for null terminator
-        if (code == NULL) 
+        char *code = new char[fileSize + 1];
+        if (code == nullptr)
         {
             perror("Error allocating memory");
             fclose(file);
-            return NULL;
+            return nullptr;
         }
 
         size_t bytesRead = fread(code, sizeof(char), fileSize, file);
-        if (bytesRead != fileSize) 
+        if (bytesRead != fileSize)
         {
             perror("Error reading file");
-            free(code);
+            delete[] code; // Free memory on error
             fclose(file);
-            return NULL;
+            return nullptr;
         }
 
-        code[fileSize] = '\0'; // Null-terminate the string
+        code[fileSize] = '\0';
 
         fclose(file);
         return code;
@@ -96,70 +94,79 @@ public:
     {
     }
 
-    /// @brief This is the method to load a file \name load
-    /// @param filename This is the name of the file to be loaded
-    /// @return This will return 0 for success and 1 for failure
-    int load(const char *filename) 
+    /// @brief This is the load method, to load the file \name load
+    /// @param filename 
+    /// @return 
+    int load(const char *filename)
     {
-        // Check if the file is already loaded
-        if (page.find(filename) != page.end()) 
+        if (page.find(filename) != page.end())
         {
             printf("File '%s' is already loaded.\n", filename);
             return 0;
         }
 
         char *fileContent = readFile(filename);
-        if (fileContent == NULL) 
+        if (fileContent == nullptr)
         {
             printf("Error loading file '%s'.\n", filename);
             return 0;
         }
 
-        page[filename] = fileContent;
+        page[filename] = std::unique_ptr<char[]>(fileContent);
         return 1;
     }
 
-    /// @brief This is the method to get a page \name get_page
-    /// @param filename This is the name of the file to be loaded
-    /// @param request_type This is the type of request
-    /// @param input This is the input to be added to the dictionary
-    /// @param text This is the text to be checked in the dictionary
+    /// @brief This is the get page method, to get the page \name get_page
+    /// @param filename This is the file name, which is to be loaded
+    /// @param request_type This is the request type, get => 0 , post => 1 , put=> 2
+    /// @param input This is the input string
+    /// @param text This is the text string
     /// @return This will return the page
     const char *get_page(const char *filename, int request_type, std::string input, std::string text)
     {
-        if (std::fstream(filename).fail())
+        if (std::filesystem::exists(filename))
+        {
+            std::string str = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=ISO-8859-1\r\n\r\n" + std::string(page[filename].get());
+            if (request_type == 1)
+            {
+                add_dictionary(text);
+            }
+            else if (request_type == 0 && !input.empty())
+            {
+                int is_contains = check_dictionary(input);
+                if (is_contains)
+                {
+                    input += " is found in your Dictionary";
+                    size_t pos = str.find("<!Rvalue>");
+                    if (pos != std::string::npos)
+                    {
+                        str.replace(pos, 9, input);
+                    }
+                }
+                else
+                {
+                    input += " is NOT found in your Dictionary";
+                    size_t pos = str.find("<!Rvalue>");
+                    if (pos != std::string::npos)
+                    {
+                        str.replace(pos, 9, input);
+                    }
+                }
+            }
+
+            char *cstr = new char[str.length() + 1];
+            strcpy(cstr, str.c_str());
+            return cstr;
+        }
+        else
         {
             std::cerr << filename << " could not be opened! Check if the path is correct\n";
             return "";
         }
-
-        std::string str = "HTTP/1.1 200 Okay\r\nContent-Type: text/html; charset=ISO-8859-4 \r\n\r\n" + std::string(page[filename]);
-        if (request_type == 1)
-        {
-            add_dictionary(text);
-        }
-        else if (request_type == 0 && input != "")
-        {
-            int is_contains = check_dictionary(input);
-            if (is_contains)
-            {
-                input += " is found in your Dictionary";
-                str.replace(str.find("<!Rvalue>"), 9, input.c_str());
-            }
-            else
-            {
-                input += " is NOT found in your Dictionary";
-                str.replace(str.find("<!Rvalue>"), 9, input.c_str());
-            }
-        }
-
-        char *cstr = new char[str.length() + 1];
-        strcpy(cstr, str.c_str());
-        return cstr;
     }
 
-    /// @brief This is the method to add a word to the dictionary \name add_dictionary
-    /// @param word This is the word to be added to the dictionary
+    /// @brief This is the add dictionary method, to add the dictionary \name add_dictionary
+    /// @param word This is the word, which is to be added
     void add_dictionary(std::string word)
     {
         if (word.empty())
@@ -168,39 +175,36 @@ public:
         DictionaryLock.lock();
         if (dictionary.count(word) == 0)
         {
-            // Add the word to the dictionary set
             dictionary.insert(word);
 
-            // Save the dictionary data to the "DATABASE.xml" file
             int saveResult = saveDataToXMLDatabase("DATABASE.xml");
-            if (saveResult == 0) 
+            if (saveResult == 0)
             {
                 std::cout << "Data saved to DATABASE.xml" << std::endl;
-            } 
-            else 
+            }
+            else
             {
                 std::cerr << "Failed to save data to DATABASE.xml" << std::endl;
             }
 
-            // Save the word to the "dictionary.txt" file
             std::ofstream fDictionary("dictionary.txt", std::ios::app);
-            if (!fDictionary.good())
+            if (fDictionary.good())
             {
-                std::cerr << "Failed to open file\n";
+                fDictionary << word << "\n";
                 fDictionary.close();
             }
+            else
+            {
+                std::cerr << "Failed to open file\n";
+            }
 
-            fDictionary << word.c_str() << "\n";
-            fDictionary.close();
+            DictionaryLock.unlock();
         }
-
-        DictionaryLock.unlock();
     }
 
-
-    /// @brief This is the method to check if a word is in the dictionary \name check_dictionary
-    /// @param word This is the word to be checked in the dictionary
-    /// @return This is the return value of the method
+    /// @brief This is the check dictionary method, to check the dictionary \name check_dictionary
+    /// @param word This is the word, which is to be checked
+    /// @return This will return the return value
     int check_dictionary(std::string word)
     {
         DictionaryLock.lock();
@@ -209,7 +213,7 @@ public:
         return return_value;
     }
 
-    /// @brief This is the method to initialize the dictionary \name init_dictionary
+    /// @brief This is the init dictionary method, to initialize the dictionary \name init_dictionary
     void init_dictionary()
     {
         DictionaryLock.lock();
@@ -248,72 +252,65 @@ public:
         DictionaryLock.unlock();
     }
 
-    /// @brief This is the method to save data to a file \name saveDataToXMLDatabase
-    /// @param filename This is the name of the file to be saved
-    /// @return This will return 0 for success and 1 for failure
-    int saveDataToXMLDatabase(const std::string& filename)
+    /// @brief This is the save data to XML database method, to save the data to XML database \name saveDataToXMLDatabase
+    /// @param filename This is the file name, which is to be saved
+    /// @return This will 0 if the data is saved, and 1 if the data is not saved
+    int saveDataToXMLDatabase(const std::string &filename)
     {
         std::ofstream file(filename);
         if (file.is_open())
         {
             int entryNumber = 1;
-            // TODO: check if there is already the xml structure, if yes only add the new word, if not go on with the normal procedure
-            // Write the XML header
             file << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << std::endl;
-            // Start the root element
             file << "<Database>" << std::endl;
 
-            for (const std::string& item : dictionary) 
+            for (const std::string &item : dictionary)
             {
-                // Check if the item is empty or contains only whitespace
-                if (!item.empty() && !std::all_of(item.begin(), item.end(), ::isspace)) 
+                if (!item.empty() && !std::all_of(item.begin(), item.end(), ::isspace))
                 {
-                    // Write each non-empty and non-whitespace word as an XML element
                     file << "  <Entry_" << entryNumber << ">" << item << "</Entry_" << entryNumber << ">" << std::endl;
                     entryNumber++;
                 }
             }
 
-            // End the root element
             file << "</Database>" << std::endl;
             file.close();
-            return 0; // Success
-        } 
-        else 
+            return 0;
+        }
+        else
         {
             std::cerr << "Error opening file for saving data." << std::endl;
-            return 1; // Failure
+            return 1;
         }
     }
 
-    /// @brief This is the method to load data from a file \name loadDataFromXMLDatabase
-    /// @param filename This is the name of the file to be loaded
-    /// @return This will return 0 for success and 1 for failure
-    int loadDataFromXMLDatabase(const std::string& filename)
+    /// @brief This is the load data from XML database method, to load the data from XML database \name loadDataFromXMLDatabase
+    /// @param filename This is the file name, which is to be loaded
+    /// @return This will 0 if the data is loaded, and 1 if the data is not loaded
+    int loadDataFromXMLDatabase(const std::string &filename)
     {
         std::ifstream file(filename);
-        if (file.is_open()) 
+        if (file.is_open())
         {
             std::string line;
 
-            while (std::getline(file, line)) 
+            while (std::getline(file, line))
             {
-                // Parse XML elements and extract words
-                if (line.find("<Entry>") != std::string::npos &&
-                    line.find("</Entry>") != std::string::npos)
+                if (line.find("<Entry_") != std::string::npos &&
+                    line.find("</Entry_") != std::string::npos)
                 {
-                    std::string word = line.substr(line.find("<Entry>") + 6, line.find("</Word>") - 6);
+                    std::string word = line.substr(line.find("<Entry_") + 7, line.find("</Entry_") - 7);
                     dictionary.insert(word);
                 }
             }
 
             file.close();
-            return 0; // Success
-        } 
-        else 
+            return 0;
+        }
+        else
         {
             std::cerr << "Error opening file for loading data." << std::endl;
-            return 1; // Failure
+            return 1;
         }
     }
 };
@@ -321,7 +318,6 @@ public:
 website_handler website;
 website_handler API;
 
-/// @brief This is the class for the server \name server
 class server
 {
 private:
@@ -331,8 +327,8 @@ private:
     struct sockaddr_in address;
     int server_up;
 
-    /// @brief This is the method to create a new socket \name new_socket
-    /// @return This will return 0 for success and -1 for failure
+    /// @brief This is the new socket method, to create a new socket \name new_socket
+    /// @return This will return 0 if the socket is created, and -1 if the socket is not created
     int new_socket()
     {
         file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
@@ -344,8 +340,8 @@ private:
         return 0;
     }
 
-    /// @brief  This is the method to bind the address \name bind_address
-    /// @return This will return 0 for success and -1 for failure
+    /// @brief This is the bind address method, to bind the address \name bind_address
+    /// @return This will return 0 if the address is binded, and -1 if the address is not binded
     int bind_address()
     {
         int return_value = bind(file_descriptor, (struct sockaddr *)&address, sizeof(address));
@@ -357,9 +353,9 @@ private:
         return 0;
     }
 
-    /// @brief This is the method to start listening \name start_listen
-    /// @param k This is the number of connections to be listened to
-    /// @return This will return 0 for success and -1 for failure
+    /// @brief This is the start listen method, to start listening \name start_listen
+    /// @param k This is the number of connections
+    /// @return This will return 0 if the listening is started, and -1 if the listening is not started
     int start_listen(int k = 100000)
     {
         int return_value = listen(file_descriptor, k);
@@ -371,15 +367,14 @@ private:
         return 0;
     }
 
-    /// @brief This is the method to accept a connection \name accept_connection
-    /// @return This will return the connection value if successful and -1 for failure
+    /// @brief This is the accept connection method, to accept the connection \name accept_connection
     int accept_connection()
     {
-	#if defined(__unix__) || defined(__APPLE__)
+        #if defined(__unix__) || defined(__APPLE__)
         int connection_value = accept(file_descriptor, (struct sockaddr *)&address, (socklen_t *)&sizeof_address);
-	#else
-		int connection_value = accept(file_descriptor, (struct sockaddr *)&address, &sizeof_address);
-	#endif
+        #else
+        int connection_value = accept(file_descriptor, (struct sockaddr *)&address, &sizeof_address);
+        #endif
         if (connection_value < 0)
         {
             perror("ERROR: Connection Accept Failure");
@@ -390,9 +385,9 @@ private:
 
 public:
     /// @brief This is the constructor for the server \name server
-    /// @param internet_address This is the internet address
-    /// @param port_number This is the port number
-    /// @param THREAD_COUNT This is the number of threads
+    /// @param internet_address This is the internet address, which is to be used
+    /// @param port_number This is the port number, which is to be used
+    /// @param THREAD_COUNT This is the thread count, which is to be used
     server(int internet_address, int port_number = 80, int THREAD_COUNT = 10)
     {
         this->THREAD_COUNT = THREAD_COUNT;
@@ -415,30 +410,25 @@ public:
         server_up = 1;
     }
 
+    /// @brief This is the destructor for the server \name ~server
     ~server()
     {
         shutdown(file_descriptor, 2);
     }
 
-    /// @brief This is the method to create a new thread \name connection_thread
-    /// @param argv This is the argument to the thread
-    /// @return This will return NULL if failed, else it will return the thread
+    /// @brief This is the connection thread method, to create a connection thread \name connection_thread
+    /// @param argv This is the argument
+    /// @return This will return NULL
     static void *connection_thread(void *argv)
     {
         while (true)
         {
-            int socket_num;
-            QueueLock.lock();
-            if (!event_queue.empty())
+            int socket_num = -1;
             {
+                std::unique_lock<std::mutex> lock(QueueLock);
+                cv.wait(lock, [] { return !event_queue.empty(); });
                 socket_num = event_queue.front();
                 event_queue.pop();
-                QueueLock.unlock();
-            }
-            else
-            {
-                QueueLock.unlock();
-                continue;
             }
 
             char buffer[1000] = {0};
@@ -448,7 +438,12 @@ public:
             if (buffer_length < 0)
             {
                 perror("ERROR: Receiving Failure");
-                return NULL;
+                #ifdef __win32__
+                closesocket(socket_num);
+                #else
+                close(socket_num);
+                #endif
+                continue;
             }
 
             html::html_parser request(buffer, buffer_length);
@@ -459,17 +454,17 @@ public:
             if (send_value < 0)
             {
                 perror("ERROR: Sending Failure");
-                return NULL;
             }
-		#if defined(__unix__) || defined(__APPLE__)
+            
+            #ifdef __win32__
+            closesocket(socket_num);
+            #else
             close(socket_num);
-		#else
-			closesocket(socket_num);
-		#endif
+            #endif
         }
     }
 
-    /// @brief This is the method to start the server \name start
+    /// @brief This is the start method, to start the server \name start
     void start()
     {
         if (server_up == 0)
@@ -487,17 +482,19 @@ public:
         while (1)
         {
             int socket_num = accept_connection();
-            QueueLock.lock();
-            event_queue.push(socket_num);
-            QueueLock.unlock();
+            {
+                std::lock_guard<std::mutex> lock(QueueLock);
+                event_queue.push(socket_num);
+            }
+            cv.notify_one();
         }
     }
 };
 
-/// @brief This is the main function \name main
-/// @param argc This is the number of command-line arguments
-/// @param argv This is an array of command-line arguments
-/// @return This will return 0 for success and 1 for failure
+/// @brief This is the main function \name main, this will be used to run the server
+/// @param argc This is the argument count
+/// @param argv This is the argument
+/// @return This will return 0 if the server is started, and 1 if the server is not started
 int main(int argc, char **argv)
 {
     #ifdef _WIN32
