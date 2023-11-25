3f35725fc690c8c56e0a1087f5eeeb3af78fb6e1
Adrian <52537419+mendax0110@users.noreply.github.com>
2023-11-25 20:23:19 +0100

    updated doxygen docu

--- docs/doxygen/html/menudata.js
+++ docs/doxygen/html/menudata.js
@@ -37,24 +37,8 @@ var menudata={children:[
 {text:"Class Index",url:"classes.html"},
 {text:"Class Hierarchy",url:"hierarchy.html"},
 {text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html",children:[
-{text:"c",url:"functions.html#index_c"},
-{text:"f",url:"functions.html#index_f"},
-{text:"g",url:"functions.html#index_g"},
-{text:"h",url:"functions.html#index_h"},
-{text:"p",url:"functions.html#index_p"},
-{text:"r",url:"functions.html#index_r"},
-{text:"s",url:"functions.html#index_s"},
-{text:"t",url:"functions.html#index_t"}]},
-{text:"Functions",url:"functions_func.html",children:[
-{text:"c",url:"functions_func.html#index_c"},
-{text:"f",url:"functions_func.html#index_f"},
-{text:"g",url:"functions_func.html#index_g"},
-{text:"h",url:"functions_func.html#index_h"},
-{text:"p",url:"functions_func.html#index_p"},
-{text:"r",url:"functions_func.html#index_r"},
-{text:"s",url:"functions_func.html#index_s"},
-{text:"t",url:"functions_func.html#index_t"}]}]}]},
+{text:"All",url:"functions.html"},
+{text:"Functions",url:"functions_func.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
--- docs/doxygen/html/search/functions_7.js
+++ docs/doxygen/html/search/functions_7.js
@@ -1,15 +1,13 @@
 var searchData=
 [
-  ['sanitizelogicmemory_0',['sanitizeLogicMemory',['../classsanitizer_1_1sanitizer_internals.html#a61761c72a274989def2b3580678664b8',1,'sanitizer::sanitizerInternals']]],
-  ['sanitizelogicthread_1',['sanitizeLogicThread',['../classsanitizer_1_1sanitizer_internals.html#a731a836c431fed221854474b0da2f00f',1,'sanitizer::sanitizerInternals']]],
-  ['setheader_2',['SetHeader',['../classatomizes_1_1_h_t_t_p_message.html#a6de4f3704ca9e960cd4fb33ea5a18212',1,'atomizes::HTTPMessage']]],
-  ['setheaders_3',['SetHeaders',['../classatomizes_1_1_h_t_t_p_message.html#a38da70c8cb5c6d2f082cf05f5eb8efd9',1,'atomizes::HTTPMessage']]],
-  ['setmessagebody_4',['setmessagebody',['../classatomizes_1_1_h_t_t_p_message.html#a8f472f655c27f46f5b7fcc5dc822b7db',1,'atomizes::HTTPMessage::SetMessageBody(const std::string &amp;body)'],['../classatomizes_1_1_h_t_t_p_message.html#a404ca1c05541eef401c8e39549293f42',1,'atomizes::HTTPMessage::SetMessageBody(const std::vector&lt; uint8_t &gt; &amp;body)']]],
-  ['setmethod_5',['SetMethod',['../classatomizes_1_1_h_t_t_p_message.html#a90d008288e413a9654347268e53df5bc',1,'atomizes::HTTPMessage']]],
-  ['setpath_6',['SetPath',['../classatomizes_1_1_h_t_t_p_message.html#a87dd87e773e87eb5b267134a1f0f1f94',1,'atomizes::HTTPMessage']]],
-  ['setstatuscode_7',['SetStatusCode',['../classatomizes_1_1_h_t_t_p_message.html#ab9acdf6e53b813852210e7a1e67ace28',1,'atomizes::HTTPMessage']]],
-  ['setstatusmessage_8',['SetStatusMessage',['../classatomizes_1_1_h_t_t_p_message.html#a521890068dc9e7536a615bb1b2ff7718',1,'atomizes::HTTPMessage']]],
-  ['setversion_9',['SetVersion',['../classatomizes_1_1_h_t_t_p_message.html#a5b31c70ce7d341614ac6fa6e21bd1320',1,'atomizes::HTTPMessage']]],
-  ['statustextfromstatuscode_10',['StatusTextFromStatusCode',['../namespaceatomizes.html#aa6725ff91253e4c7d71d8331d999bcb9',1,'atomizes']]],
-  ['stringtomessagemethod_11',['StringToMessageMethod',['../namespaceatomizes.html#a143ff5a0b4d02d94552b9820e2fabc6a',1,'atomizes']]]
+  ['setheader_0',['SetHeader',['../classatomizes_1_1_h_t_t_p_message.html#a6de4f3704ca9e960cd4fb33ea5a18212',1,'atomizes::HTTPMessage']]],
+  ['setheaders_1',['SetHeaders',['../classatomizes_1_1_h_t_t_p_message.html#a38da70c8cb5c6d2f082cf05f5eb8efd9',1,'atomizes::HTTPMessage']]],
+  ['setmessagebody_2',['setmessagebody',['../classatomizes_1_1_h_t_t_p_message.html#a8f472f655c27f46f5b7fcc5dc822b7db',1,'atomizes::HTTPMessage::SetMessageBody(const std::string &amp;body)'],['../classatomizes_1_1_h_t_t_p_message.html#a404ca1c05541eef401c8e39549293f42',1,'atomizes::HTTPMessage::SetMessageBody(const std::vector&lt; uint8_t &gt; &amp;body)']]],
+  ['setmethod_3',['SetMethod',['../classatomizes_1_1_h_t_t_p_message.html#a90d008288e413a9654347268e53df5bc',1,'atomizes::HTTPMessage']]],
+  ['setpath_4',['SetPath',['../classatomizes_1_1_h_t_t_p_message.html#a87dd87e773e87eb5b267134a1f0f1f94',1,'atomizes::HTTPMessage']]],
+  ['setstatuscode_5',['SetStatusCode',['../classatomizes_1_1_h_t_t_p_message.html#ab9acdf6e53b813852210e7a1e67ace28',1,'atomizes::HTTPMessage']]],
+  ['setstatusmessage_6',['SetStatusMessage',['../classatomizes_1_1_h_t_t_p_message.html#a521890068dc9e7536a615bb1b2ff7718',1,'atomizes::HTTPMessage']]],
+  ['setversion_7',['SetVersion',['../classatomizes_1_1_h_t_t_p_message.html#a5b31c70ce7d341614ac6fa6e21bd1320',1,'atomizes::HTTPMessage']]],
+  ['statustextfromstatuscode_8',['StatusTextFromStatusCode',['../namespaceatomizes.html#aa6725ff91253e4c7d71d8331d999bcb9',1,'atomizes']]],
+  ['stringtomessagemethod_9',['StringToMessageMethod',['../namespaceatomizes.html#a143ff5a0b4d02d94552b9820e2fabc6a',1,'atomizes']]]
 ];
